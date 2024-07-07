94b3c5c1b4b207f5436c902b07c721f356462808
Govinda <143345467+Govinda-Upadhya@users.noreply.github.com>
2024-07-07 16:20:45 +0530

    Update settings.py

--- Dimen/settings.py
+++ Dimen/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),os.path.join(BASE_DIR,'dist/assets')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_URL="/user-media/"
 MEDIA_ROOT=BASE_DIR/'Images'
 
